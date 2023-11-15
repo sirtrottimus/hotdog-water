c7f3bc77bcde15c90838aa3ded29c6dc925836d8
DanB-dev <20670262+DanB-dev@users.noreply.github.com>
2023-11-15 16:54:52 +0000

    Update useBackendSocket.ts

--- frontend/src/hooks/useBackendSocket.ts
+++ frontend/src/hooks/useBackendSocket.ts
@@ -85,7 +85,8 @@ const useBackendSocket = () => {
                 (event.data.provider &&
                   event.data.provider === 'youtube' &&
                   event.data.type !== 'subscriber') ||
-                (event.data.provider === 'twitch' &&
+                (event.data.provider &&
+                  event.data.provider === 'twitch' &&
                   event.data.type !== 'follow')
             );
             return [...sortedData, { eventName: 'event', ...data }];
