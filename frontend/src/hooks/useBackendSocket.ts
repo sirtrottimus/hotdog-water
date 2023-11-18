edee18d8296a2e80c85304ce707570f9d26ab6c7
DanB-dev <20670262+DanB-dev@users.noreply.github.com>
2023-11-18 17:40:08 +0000

    Update useBackendSocket.ts

--- frontend/src/hooks/useBackendSocket.ts
+++ frontend/src/hooks/useBackendSocket.ts
@@ -124,7 +124,7 @@ const useBackendSocket = () => {
 
                 if (!provider || !type) {
                   console.log('Removed event: ', event, 'because of no type');
-                  return true; // Remove the event
+                  return false; // Remove the event
                 } // Remove the event
 
                 if (
