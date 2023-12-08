a67f1c6476ca78a0dc4179e09a3dda7e874453a4
DanB-dev <20670262+DanB-dev@users.noreply.github.com>
2023-12-08 14:10:10 +0000

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,12 +7,6 @@ const nextConfig = {
 
     return config;
   },
-  env: {
-    PROD_API_URL: 'https://api.hatfilms.co.uk',
-    DEV_API_URL: 'http://localhost:6969',
-    NEXT_PUBLIC_VERSION: '1.5.4',
-    NEXT_PUBLIC_NAME: 'Hat Films',
-  },
 };
 
 export default nextConfig;
