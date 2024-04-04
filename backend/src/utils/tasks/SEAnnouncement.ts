a3bab48f4ed684c3eaec106fcfff759f119159b3
DanB-dev <20670262+DanB-dev@users.noreply.github.com>
2024-04-04 17:54:38 +0100

    Add funny quips to SEAnnouncement task

--- backend/src/utils/tasks/SEAnnouncement.ts
+++ backend/src/utils/tasks/SEAnnouncement.ts
@@ -16,6 +16,19 @@ const fetchActivity = async (): Promise<any> => {
   else return fetchActivity();
 };
 
+const funnyQuips = [
+  "You're banned.",
+  'You disgust me.',
+  "You're not welcome here.",
+  "You're a terrible person.",
+  "You're a terrible viewer.",
+  "I can't believe you're still here.",
+  'You really wrote that in chat?',
+  'You really thought that was funny?',
+  "That's just not funny.",
+  "You can't joke about that anymore.",
+];
+
 // post the activity to SE
 const postAnnouncement = async () => {
   console.log('Posting announcement to SE');
@@ -25,7 +38,7 @@ const postAnnouncement = async () => {
   }
   const announcementText = `Activity will be read out intermittently during the stream. (Except you ${
     random.data.username ?? random.data.displayName
-  }, you're banned.)`;
+  }, ${funnyQuips[Math.floor(Math.random() * funnyQuips.length)]})`;
   const postTo = ['Twitch (StreamElements)'];
 
   console.log('Announcement:', announcementText);
